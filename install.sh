8c9e2e1fde998b685db84955f33e997e19883300
Suryansh <suryansh5060@gmail.com>
2023-04-29 03:56:01 +0530

    Add autocomplete and whitespace for my sanity

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 chmod +x shaman
 cp shaman ~/.local/bin/
+
+echo 'eval "$(register-python-argcomplete shaman)"' >> ~/.bashrc
+
+FILE=~/.zshrc
+if [[ -f "$FILE" ]]; then
+    echo "$FILE exists."
+    echo 'eval "$(register-python-argcomplete shaman)"' >> ~/.zshrc
+fi
